acc91e483556f18762b9f8cd4a9a2633c24394c3
Vladislav <vladkkon@mail.ru>
2024-04-09 20:03:14 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Author tolstoy = new Author("Лев", "Толстой");
+        Book book1 = new Book("Анна Каренина", tolstoy, 1960);
+        System.out.println(book1.getTitle() + " " + book1.getAuthor().getName() + " " + book1.getAuthor().getLastName() + " " + book1.getPublicationYear());
+        book1.setPublicationYear(1980);
+        System.out.println(book1.getTitle() + " " + book1.getAuthor().getName() + " " + book1.getAuthor().getLastName() + " " + book1.getPublicationYear());
+
+        Author pushkin = new Author("Александр", "Пушкин");
+        Book book2 = new Book("Евгений Онегин", pushkin, 1973);
+        System.out.println(book2.getTitle() + " " + book2.getAuthor().getName() + " " + book2.getAuthor().getLastName() + " " + book2.getPublicationYear());
+
     }
 }
